05e041d36f68cf668496b5c0acccb2f92d269882
CheolminChoi <cheolmin.choi@pabii.co.kr>
2020-08-28 16:55:08 +0900

    잣갓띠 똥

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./component/app/app";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <App />,
+    // </React.StrictMode>,
     document.getElementById("root"),
 );
 
